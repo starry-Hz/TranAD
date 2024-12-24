2842d6fe0070d944e5014d9f905a0e877409d5e9
starry-Hz <hzhao1213@163.com>
2024-12-24 20:30:23 +0800

    修改plt的style

--- src/plotting.py
+++ src/plotting.py
@@ -4,7 +4,8 @@ import statistics
 import os, torch
 import numpy as np
 
-plt.style.use(['science', 'ieee'])
+# plt.style.use(['science', 'ieee'])
+plt.style.use('default')
 plt.rcParams["text.usetex"] = False
 plt.rcParams['figure.figsize'] = 6, 2
 
